17a65d632f99be4d81fd1df6c7510ce281f53473
Matthew Burrows <mattjburrows@gmail.com>
2013-10-16 22:51:56 +0100

    Pulling in github repos and tweets

--- js/app.js
+++ js/app.js
@@ -1,7 +1,13 @@
 (function($, App){
 
+    App.prototype._config = {
+        reposUrl: 'https://api.github.com/users/mattjburrows/repos?callback=?',
+        perPage: '&per_page=100'
+    };
+
 	App.prototype.init = function(){
 		this.fetchTweets();
+        this.fetchRepositories();
 		return this;
 	};
 
